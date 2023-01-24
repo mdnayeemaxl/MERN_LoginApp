e7105452f98b66da561728c13aef1ce1d597e43d
Md Nayeem <56497545+mdnayeemaxl@users.noreply.github.com>
2023-01-24 12:52:27 +0600

    Mail generator Done

--- server/router/route.js
+++ server/router/route.js
@@ -3,9 +3,10 @@ const router = Router();
 
 import * as controller from '../controllers/appController.js'
 import Auth,{localVariables} from "../middleware/auth.js"
+import { registerMail } from "../controllers/Mailer.js";
 
 router.route('/register').post(controller.register);
-router.route('/registerMail').post();
+router.route('/registerMail').post(registerMail);
 router.route('/authenticate').post();
 router.route('/login').post(controller.verifyUser,controller.login);
 
@@ -16,7 +17,7 @@ router.route('/verifyOTP').get(controller.verifyOTP);
 router.route('createResetSession').get();
 
 router.route('/updateuser').put(Auth,controller.updateUser);
-router.route('/resetPassword').get();
+router.route('/resetPassword').put(controller.verifyUser,controller.resetPassword);
 
 
 export default router;
